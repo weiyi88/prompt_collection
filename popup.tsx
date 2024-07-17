f055eeeb6da1227bc36f3559a34eb03401343421
Aring <tantubuping@163.com>
2024-07-17 19:03:52 +0800

    first try

--- popup.tsx
+++ popup.tsx
@@ -8,17 +8,12 @@ function IndexPopup() {
       style={{
         padding: 16
       }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+      <ul>
+        <li>第一项</li>
+      </ul>
+      <ul>
+        <li>第二项</li>
+      </ul>
     </div>
   )
 }
